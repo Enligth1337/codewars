24addfe2e2785681f89534ff8b97c4dce79f2f9a
Enligth1337 <bocharov647@gmail.com>
2023-07-30 21:48:03 +0600

    new kata

--- kyu7.go
+++ kyu7.go
@@ -8,7 +8,7 @@ import (
 	"unicode"
 )
 
-//https://www.codewars.com/kata/559590633066759614000063
+// https://www.codewars.com/kata/559590633066759614000063
 func MinMax(arr []int) [2]int {
 	//mm := map[string]int{"min": arr[0], "max": arr[0]}
 	//for _, a := range arr {
@@ -24,7 +24,7 @@ func MinMax(arr []int) [2]int {
 	return [2]int{arr[0], arr[len(arr)-1]}
 }
 
-//https://www.codewars.com/kata/54ff3102c1bad923760001f3
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
 func GetCount(str string) (count int) {
 	// Enter solution here
 	var res int
@@ -37,7 +37,7 @@ func GetCount(str string) (count int) {
 	return res
 }
 
-//https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 func Accum(s string) string {
 	// your code
 	var res []string
@@ -47,7 +47,7 @@ func Accum(s string) string {
 	return strings.Join(res, "-")
 }
 
-//https://www.codewars.com/users/Enligth1337/completed_solutions
+// https://www.codewars.com/users/Enligth1337/completed_solutions
 func NbYear(p0 int, percent float64, aug int, p int) int {
 	// your code
 	curr := p0
@@ -59,7 +59,7 @@ func NbYear(p0 int, percent float64, aug int, p int) int {
 	return res
 }
 
-//https://www.codewars.com/kata/56541980fa08ab47a0000040
+// https://www.codewars.com/kata/56541980fa08ab47a0000040
 func PrinterError(s string) string {
 	var cnt int
 	for _, b := range s {
@@ -70,7 +70,7 @@ func PrinterError(s string) string {
 	return fmt.Sprintf("%d/%d", cnt, len(s))
 }
 
-//https://www.codewars.com/kata/52fba66badcd10859f00097e
+// https://www.codewars.com/kata/52fba66badcd10859f00097e
 func Disemvowel(comment string) string {
 	var res string
 	vowels := "aeiouAEIOU"
@@ -83,7 +83,7 @@ func Disemvowel(comment string) string {
 	return res
 }
 
-//https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1
+// https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1
 func Capitalize(st string, arr []int) string {
 	s := []rune(st)
 	for _, b := range arr {
@@ -94,7 +94,7 @@ func Capitalize(st string, arr []int) string {
 	return string(s)
 }
 
-//https://www.codewars.com/kata/56747fd5cb988479af000028
+// https://www.codewars.com/kata/56747fd5cb988479af000028
 func GetMiddle(s string) string {
 	//if len(s)%2 == 0 {
 	//	return s[len(s)/2-1 : len(s)/2+1]
@@ -107,19 +107,19 @@ func GetMiddle(s string) string {
 	return s[(n-1)/2 : n/2+1]
 }
 
-//https://www.codewars.com/kata/56606694ec01347ce800001b
+// https://www.codewars.com/kata/56606694ec01347ce800001b
 func IsTriangle(a, b, c int) bool {
 	m := []int{a, b, c}
 	sort.Ints(m)
 	return m[2] < m[0]+m[1]
 }
 
-//https://www.codewars.com/kata/526c7363236867513f0005ca
+// https://www.codewars.com/kata/526c7363236867513f0005ca
 func IsLeapYear(year int) bool {
 	return year%400 == 0 || (year%4 == 0 && year%100 != 0)
 }
 
-//https://www.codewars.com/kata/58941fec8afa3618c9000184
+// https://www.codewars.com/kata/58941fec8afa3618c9000184
 func GrowingPlant(upSpeed, downSpeed, desiredHeight int) (res int) {
 	curr := upSpeed
 	for curr < desiredHeight {
@@ -129,7 +129,7 @@ func GrowingPlant(upSpeed, downSpeed, desiredHeight int) (res int) {
 	return res + 1
 }
 
-//https://www.codewars.com/kata/554b4ac871d6813a03000035
+// https://www.codewars.com/kata/554b4ac871d6813a03000035
 func HighAndLow(in string) string {
 	// Code here or
 	s := strings.Split(in, " ")
@@ -142,7 +142,7 @@ func HighAndLow(in string) string {
 	return fmt.Sprintf("%d %d", arr[len(arr)-1], arr[0])
 }
 
-//https://www.codewars.com/kata/571c1e847beb0a8f8900153d
+// https://www.codewars.com/kata/571c1e847beb0a8f8900153d
 func RakeGarden(garden string) string {
 	s := strings.Split(garden, " ")
 	for i := range s {
@@ -150,6 +150,22 @@ func RakeGarden(garden string) string {
 			continue
 		}
 		s[i] = "gravel"
+	}
+	return strings.Join(s, " ")
+}
+
+// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
+func reverse(s string) (res string) {
+	for _, v := range s {
+		res = string(v) + res
+	}
+	return
+}
+
+func ReverseWords(str string) string {
+	s := strings.Split(str, " ")
+	for i, b := range s {
+		s[i] = reverse(b)
 	}
 	return strings.Join(s, " ")
 }
